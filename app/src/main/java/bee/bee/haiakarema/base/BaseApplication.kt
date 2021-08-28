9693617cb330abe5c0e82c66a94e42bc3143aa34
George <gorgonady.gn@gmail.com>
2021-08-28 19:30:57 +0200

    init bug fixes

--- app/src/main/java/bee/bee/haiakarema/base/BaseApplication.kt
+++ app/src/main/java/bee/bee/haiakarema/base/BaseApplication.kt
@@ -7,12 +7,12 @@ import com.nostra13.universalimageloader.core.ImageLoaderConfiguration
 class BaseApplication : Application() {
 
     companion object {
-        lateinit var instance: BaseApplication
+        lateinit var application: BaseApplication
     }
 
     override fun onCreate() {
         super.onCreate()
-        instance = this
+        application = this
         val config: ImageLoaderConfiguration = ImageLoaderConfiguration.Builder(this)
             .build()
         ImageLoader.getInstance().init(config)
